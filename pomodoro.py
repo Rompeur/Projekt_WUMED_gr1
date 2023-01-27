f3ba8dd7a46653e2b10ba6ceb0ebdce5bb741984
Zexm1 <119737399+Zexm1@users.noreply.github.com>
2023-01-27 10:37:15 +0000

    Finished init function, start_thread/timer fns.

--- pomodoro.py
+++ pomodoro.py
@@ -34,3 +34,48 @@ class Pomodoro:
 
         self.long_break_timer_label = ttk.Label(self.tab3, text="15:00", font=("Ubuntu", 50))
         self.long_break_timer_label.pack(pady=20)
+
+        #creating and naming tabs
+        self.tabs.add(self.tab1, text="Pomodoro")
+        self.tabs.add(self.tab2, text="Short Break")
+        self.tabs.add(self.tab3, text="Long Break")
+
+        #creating buttons that start, skip and reset the timer
+        self.grid_layout = ttk.Frame(self.root)
+        self.grid_layout.pack(pady=10)
+
+        self.start_button = ttk.Button(self.grid_layout, text="Start", command=self.start_timer_thread)
+        self.start_button.grid(row=0, column=0)
+
+        self.skip_button = ttk.Button(self.grid_layout, text="Skip", command=self.skip_timer)
+        self.skip_button.grid(row=0, column=1)
+
+        self.reset_button = ttk.Button(self.grid_layout, text="Reset", command=self.reset_timer)
+        self.reset_button.grid(row=0, column=2)
+
+        #creating and adding pomodoro counter
+        self.pomodoro_counter_label = ttk.Label(self.grid_layout, text="Pomodoros: 0", font=("Ubuntu", 16))
+        self.pomodoro_counter_label.grid(row=1, column=0, columnspan=3, pady=6)
+
+        self.pomodoros = 0
+        self.skipped = False
+        self.stopped = False
+        self.running = False
+
+        # runs the application so it can be interacted with limitlessly
+        self.root.mainloop()
+
+    #creating thread so the timer can run simultaneously with the app
+    def start_timer_thread(self):
+        if not self.running:
+            t = threading.Thread(target=self.start_timer)
+            t.start()
+            self.running = True
+
+    def start_timer(self):
+        #stopping other functions in case they are true
+        self.stopped = False
+        self.skipped = False
+        timer_id = self.tabs.index(self.tabs.select()) + 1
+
+        ## TODO Finish function `start_timer`
